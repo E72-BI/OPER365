0e9a272598a5fe09a2b05606f36e9cf9bbb669e9
E72-BI <contato.e72@gmail.com>
2025-04-30 15:26:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Este script é para efeitos de interação, caso deseje adicionar mais funcionalidades no futuro
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.05)';
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
